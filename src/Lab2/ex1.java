90e682aba4aa633666075379f276f98df38b509e
sam.pu <sam.puriwat@gmail.com>
2019-07-23 10:40:43 +0700

    lab 2 ex1

---
+++ src/Lab2/ex1.java
@@ -0,0 +1,49 @@
+package Lab2;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+
+        //date type in java
+        //integer
+        int x;  // x is integer
+        x = 10;
+        System.out.println("X = "+x);
+        x = 20;
+        int a, b, c = 30;
+
+        //real number
+        double n = 20.5;
+        float m = 10;
+        System.out.println(n);
+        System.out.println(m);
+
+        //charecter
+        char d = '1';
+        System.out.println(d);
+
+        //String
+        String s = "Puriwat";
+        System.out.println(s);
+        String l = "Lertkrai";
+        String fullname = s + l;
+
+        System.out.println(fullname);
+        System.out.println(fullname.length());
+
+        String t = fullname.concat("RUTS");
+
+        //boolean
+        boolean w = true;
+        boolean o = false;
+
+        System.out.println(!w);
+
+
+
+
+
+
+
+    }//main
+}//class
